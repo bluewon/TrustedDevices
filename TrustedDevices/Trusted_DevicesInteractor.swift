f7e745f95b0a1353358ecdeb333b62f217561cfe
Tran Trung Quan <quantranesc@gmail.com>
2017-07-10 09:34:12 +0700

    Done Autolayout and showdata in tableView

---
+++ TrustedDevices/Trusted_DevicesInteractor.swift
@@ -0,0 +1,14 @@
+//
+//  Trusted_DevicesInteractor.swift
+//  TrustedDevices
+//
+//  Created Quan Tran on 7/6/17.
+//  Copyright © 2017 Quan Tran. All rights reserved.
+//
+
+import UIKit
+
+class Trusted_DevicesInteractor: Trusted_DevicesInteractorProtocol {
+
+    weak var presenter: Trusted_DevicesPresenterProtocol?
+}
